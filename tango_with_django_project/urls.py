7c4923870f25a44ba13ff442e7af2c63f640a8b3
Ahmet Yucel <2604000Y@student.gla.ac.uk>
2023-01-25 07:06:15 +0000

    Solved the unshowed cat picture problem

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     #The above maps any URLs starting with rango/ to be handled by rango. include function
     path('admin/', admin.site.urls)
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
