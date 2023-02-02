178205c2638b31ea86abe0dc993203489ae3d22b
Ahmet Yucel <2604000Y@student.gla.ac.uk>
2023-02-02 06:07:29 +0000

    chapter 9 1 failure title of restricted page ?

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+LOGIN_URL = 'rango:login'
 STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
